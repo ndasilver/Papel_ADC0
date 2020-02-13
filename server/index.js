aa651e51b688c91e7e5e7db08d05a54b4d7009ae
ndasilver <ndasilver@gmail.com>
2020-02-13 11:05:30 +0200

    little changes on the packages

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const server = require("./config/server");
+// importing routes file
+const appRouter = require("./routes/routes.js");
+
+// Getting routes and we always add /api/v1 after the link
+app.use("/api/v1", appRouter);
+
+app.listen(server.port, () => {
+    console.log(`serving on port ${server.port}`);
+});
