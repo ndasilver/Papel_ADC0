ddbf4534e24704e4f02d838057f1dc561461929d
ndasilver <ndasilver@gmail.com>
2020-02-14 21:56:37 +0200

    chor(server settings)

---
+++ server/config/server.js
@@ -0,0 +1,12 @@
+const port = process.env.port || 3000;
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "papel",
+  password: "ndasilver",
+  port: 5432
+});
+
+module.exports = {
+  port
+};
